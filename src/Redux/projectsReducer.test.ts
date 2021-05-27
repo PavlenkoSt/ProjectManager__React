ea4901e9c618897b7285b066793e3dd7c7f9315b
Stanislav <verbatim8872@gmail.com>
2021-05-27 14:55:31 +0300

    add testing action creators in projectReducer

--- src/Redux/projectsReducer.test.ts
+++ src/Redux/projectsReducer.test.ts
@@ -1,4 +1,4 @@
-import projectsReducer, {ADD_NEW_PROJECT, projectsActions} from './projectsReducer'
+import projectsReducer, {ADD_NEW_PROJECT, CHANGE_FILTER_OPTION, DELETE_PROJECT, projectsActions, SET_PROJECTS_FROM_LS} from './projectsReducer'
 
 const state = {
     projects: [
@@ -8,44 +8,87 @@ const state = {
     filterOption: 'all'
 }
 
+
 describe('actions', () => {
     it('should create an action to add new project', () => {
-      const name = 'some name'
-      const core = 'some core'
-      const desc = 'some desc'
-      const expectedAction = {
-        type: ADD_NEW_PROJECT,
-        name,
-        core,
-        desc
-      }
+        const expectedAction = {
+            type: ADD_NEW_PROJECT,
+            name: 'some name',
+            core: 'some core',
+            desc: 'some desc'
+        }
       expect(projectsActions.addNewProject('some name', 'some core', 'some desc')).toEqual(expectedAction)
     })
-  })
 
-it('projects length should be increment', () => {
-    const action = projectsActions.addNewProject('name', 'core', '')
-    const modernizedStateProjects = projectsReducer(state, action)
-    expect(modernizedStateProjects.projects.length).toBe(3)
+    it('should create an action to delete project', () => {
+        const expectedAction = {
+            type: DELETE_PROJECT,
+            id: 1
+        }
+        expect(projectsActions.deleteProject(1)).toEqual(expectedAction)
+    })
+
+    it('should create an action to change filter option', () => {
+        const expectedAction = {
+            type: CHANGE_FILTER_OPTION,
+            filterOption: 'completed'
+        }
+        expect(projectsActions.changeFilterOption('completed')).toEqual(expectedAction)
+    })
+
+    it('should create an action to add project from local storage', () => {
+        const expectedAction = {
+            type: SET_PROJECTS_FROM_LS,
+            projects: [
+                {
+                    id: 1, 
+                    name: 'some name',
+                    core: 'some core',
+                    desc: 'some desc',
+                    completed: false, 
+                    link: 'some link'
+                }
+            ]
+        }
+        expect(projectsActions.setProjectsFromLS([
+            {
+                id: 1, 
+                name: 'some name',
+                core: 'some core',
+                desc: 'some desc',
+                completed: false, 
+                link: 'some link'
+            }
+        ])).toEqual(expectedAction)
+    })
 })
 
-it('projects length should be decrement', () => {
-    const action = projectsActions.deleteProject(1)
-    const modernizedStateProjects = projectsReducer(state, action)
-    expect(modernizedStateProjects.projects.length).toBe(1)
-})
 
-it('filtered option should be change', () => {
-    const action = projectsActions.changeFilterOption('completed')
-    const modernizedStateProjects = projectsReducer(state, action)
-    expect(modernizedStateProjects.filterOption).toBe('completed')
-})
-
-it('projects should be deleted and installed new', () => {
-    const action = projectsActions.setProjectsFromLS([
-        {id: 1, core: 'new core', name: 'some name', completed: false, desc: '', link: 'some-core-some-name'}
-    ])
-    const modernizedStateProjects = projectsReducer(state, action)
-    expect(modernizedStateProjects.projects.length).toBe(1)
-    expect(modernizedStateProjects.projects[0].core).toBe('new core')
+describe('reducers', () => {
+    it('projects length should be increment', () => {
+        const action = projectsActions.addNewProject('name', 'core', '')
+        const modernizedStateProjects = projectsReducer(state, action)
+        expect(modernizedStateProjects.projects.length).toBe(3)
+    })
+    
+    it('projects length should be decrement', () => {
+        const action = projectsActions.deleteProject(1)
+        const modernizedStateProjects = projectsReducer(state, action)
+        expect(modernizedStateProjects.projects.length).toBe(1)
+    })
+    
+    it('filtered option should be change', () => {
+        const action = projectsActions.changeFilterOption('completed')
+        const modernizedStateProjects = projectsReducer(state, action)
+        expect(modernizedStateProjects.filterOption).toBe('completed')
+    })
+    
+    it('projects should be deleted and installed new', () => {
+        const action = projectsActions.setProjectsFromLS([
+            {id: 1, core: 'new core', name: 'some name', completed: false, desc: '', link: 'some-core-some-name'}
+        ])
+        const modernizedStateProjects = projectsReducer(state, action)
+        expect(modernizedStateProjects.projects.length).toBe(1)
+        expect(modernizedStateProjects.projects[0].core).toBe('new core')
+    })
 })
